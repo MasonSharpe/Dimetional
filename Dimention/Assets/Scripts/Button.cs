a3e15ab2de468bcc26f152adb832891c2a083299
Mason <115174711+MasonSharpe@users.noreply.github.com>
2023-04-21 17:55:18 -0700

    secret

---
+++ Dimention/Assets/Scripts/Button.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public int buttonIndex = 0;
+    public List<int> prerequisites = new List<int>();
+}
