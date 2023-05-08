6a825a367236daf209dbc2e68f4790baac6b4266
AradFox <115174667+AradFox@users.noreply.github.com>
2023-05-08 08:00:14 -0700

    working on camera shake for visible recoil

---
+++ Dimention/Assets/CameraShake.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public IEnumerator Shake (float duration, float magnitude)
+    {
+        //storing origional pos of camera
+        Vector3 originalPos = transform.position;
+        // keeping track of how much time has passed since start shake
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
---
+++ Dimention/Assets/testclick.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+public class testclick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem magicFlare;
+    public Animator animator;
+    public Animation BasicMagicSlash;
+    public static AudioClip blastSound;
+    public static AudioClip fireSound;
+    static AudioSource audioSrc;
+    private void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetMouseButtonDown(0))
+        {
+            testclick.PlaySound("blastSound");
+            magicFlare.Play();
+            CameraShaker.Instance.ShakeOnce(4f, 4f, .1f, 1f);
+        }   
+    }
+    public static void PlaySound(string clip)
+    {
+        switch(clip)
+        {
+            case "fire":
+                audioSrc.PlayOneShot(blastSound); break;
+                
+        }
+    }
+}
